df34fb655c78b00af0a879c948e8a2efab573bbe
George Henry <georgemarathonman@gmail.com>
2020-03-09 14:24:21 -0400

    first factorial

---
+++ factorial.py
@@ -0,0 +1,11 @@
+
+myinput = int(input("Enter your number: "))
+answer = myinput
+
+while (myinput != 1):
+    myinput -= 1
+    answer = answer * myinput
+
+print("Your factorial is : ", answer)
+
+
